9b16a7cb81d06c3e2b2bcfe11af6c262892c59c5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-18 10:55:07 +0300

    add local storage plugin

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import localStoragePlugin from './plugins/pinia/localStorage'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(localStoragePlugin)
+app.use(pinia)
 
 app.mount('#app')
